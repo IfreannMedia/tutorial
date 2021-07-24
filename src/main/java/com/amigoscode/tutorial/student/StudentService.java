68258bfb86dcc8f7e27e290292b507dfb68aeb0a
Wario <warrenlinkedin438@gmail.com>
2021-07-24 15:34:20 +0200

    added post endpoint - register new student
    added a PostMapping to the controller, and added a test http file where the request can be tessted, log the new student out

--- src/main/java/com/amigoscode/tutorial/student/StudentService.java
+++ src/main/java/com/amigoscode/tutorial/student/StudentService.java
@@ -10,16 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentService {
 
-
 	private final StudentRepository studentRepository;
 
 	@Autowired
-	public StudentService(StudentRepository studentRepository){
+	public StudentService(StudentRepository studentRepository) {
 		this.studentRepository = studentRepository;
 	}
 
-
-    public List<Student> getStudents() {
+	public List<Student> getStudents() {
 		return studentRepository.findAll();
+	}
+
+	public void addNewStudent(Student student) {
+		System.out.println(student);
 	}
 }
