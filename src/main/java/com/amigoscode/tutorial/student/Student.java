b4b97ecaea13aa51641c64a27f87f39ff488bb06
Wario <warrenlinkedin438@gmail.com>
2021-07-24 15:12:19 +0200

    Added a StudentConfig class
    to add some students, updated the Student entity class to remove the age, so that is calculated in the java code from the d.o.b

--- src/main/java/com/amigoscode/tutorial/student/Student.java
+++ src/main/java/com/amigoscode/tutorial/student/Student.java
@@ -1,6 +1,7 @@
 package com.amigoscode.tutorial.student;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table
@@ -27,6 +29,7 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
     public Student() {
@@ -38,14 +41,12 @@ public class Student {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -81,7 +82,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.getDob(), LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
