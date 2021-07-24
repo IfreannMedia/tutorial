ad63ba89928de62896b182b4093da192eba748bc
Wario <warrenlinkedin438@gmail.com>
2021-07-24 14:26:37 +0200

    added data access layer
    using the JpaRepository base interface for my StudentRepository, edited the getStudents method in the StudentService to use it.

--- src/main/java/com/amigoscode/tutorial/student/StudentRepository.java
+++ src/main/java/com/amigoscode/tutorial/student/StudentRepository.java
@@ -1,5 +1,9 @@
 package com.amigoscode.tutorial.student;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
